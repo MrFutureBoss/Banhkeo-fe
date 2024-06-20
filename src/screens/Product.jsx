57b5fdf82a480d43fe49cfba36593e644ed8cf72
MrFutureBoss <146051100+MrFutureBoss@users.noreply.github.com>
2024-06-21 03:36:48 +0700

    oke

--- src/screens/Product.jsx
+++ src/screens/Product.jsx
@@ -4,7 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import Marquee from "react-fast-marquee";
 import { useDispatch } from "react-redux";
 import { addCart } from "../redux/action";
-import { Footer, Navbar } from "../components";
+import { Navbar } from "../components";
 import { BACK_END } from "../utils/AppConfig.js";
 const Product = () => {
   const { id } = useParams();
