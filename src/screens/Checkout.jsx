096fd3657d29a3355c21e5648d0c8cf0b208f652
MrFutureBoss <146051100+MrFutureBoss@users.noreply.github.com>
2024-06-27 22:46:35 +0700

    oke

--- src/screens/Checkout.jsx
+++ src/screens/Checkout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Footer, Navbar } from "../components";
+import { Navbar } from "../components";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 const Checkout = () => {
